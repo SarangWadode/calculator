563862cf0ea21b9d3a4c5cf47e983f4c0a3e49de
Sarang Wadode <sarangwadode@gmail.com>
2022-01-02 23:32:14 +0530

    keyboard grid done

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,38 +1,31 @@
 import React, { Component } from 'react'
+import './buttons.css'
 
 export default class Buttons extends Component {
     render() {
         return (
-            <div>
-                <div className="buttons">
-                    <div className="row-one">
-                        <button id="clear">AC</button>
-                        <button id="divide">/</button>
-                        <button id="multiply">x</button>
-                    </div>
-                    <div className="row-two">
-                        <button id="seven">7</button>
-                        <button id="eight">8</button>
-                        <button id="nine">9</button>
-                        <button id="subtract">-</button>
-                    </div>
-                    <div className="row-three">
-                        <button id="four">4</button>
-                        <button id="five">5</button>
-                        <button id="six">6</button>
-                        <button id="add">+</button>
-                    </div>
-                    <div className="row-four">
-                        <button id="one">1</button>
-                        <button id="two">2</button>
-                        <button id="three">3</button>
-                        <button id="equals">=</button>
-                    </div>
-                    <div className="row-five">
-                        <button id="zero">0</button>
-                        <button id="decimal">.</button>
-                    </div>
-                </div>
+            <div className="keyboard">
+                <button id="clear">AC</button>
+                <button id="divide">/</button>
+                <button id="multiply">x</button>
+
+                <button id="seven">7</button>
+                <button id="eight">8</button>
+                <button id="nine">9</button>
+                <button id="subtract">-</button>
+
+                <button id="four">4</button>
+                <button id="five">5</button>
+                <button id="six">6</button>
+                <button id="add">+</button>
+
+                <button id="one">1</button>
+                <button id="two">2</button>
+                <button id="three">3</button>
+
+                <button id="zero">0</button>
+                <button id="decimal">.</button>
+                <button id="equals">=</button>
             </div>
         )
     }
