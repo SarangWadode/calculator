933a13534c3858076dcbcc75ba356413eb62e8ff
Sarang Wadode <sarangwadode@gmail.com>
2022-01-03 08:46:49 +0530

    grid fixed

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,32 @@ import './app.css';
 
 export default class App extends Component {
   render() {
+    const keys = [
+      {id : 'clear', number: 'AC'},
+      {id : 'divide', number: '/'},
+      {id: 'multiply', number: 'x'},
+      {id: 'seven', number: '7'},
+      {id: 'eight', number: '8'},
+      {id: 'nine', number: '9'},
+      {id: 'subtract', number: '-'},
+      {id: 'four', number: '4'},
+      {id: 'five', number: '5'},
+      {id: 'six', number: '6'},
+      {id: 'add', number: '+'},
+      {id: 'one', number: '1'},
+      {id: 'two', number: '2'},
+      {id: 'three', number: '3'},
+      {id: 'zero', number: '0'},
+      {id: 'decimal', number: '.'},
+      {id: 'equals', number: '='}
+    ]
     return (
       <div className="container">
         <div className="calculator">
           <Display />
-          <Buttons />
+          <div className="keyboard">
+            { keys.map(key => <Buttons id={key.id} number={key.number} />)}
+          </div>
         </div>
         <Footer />
       </div>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,32 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './buttons.css'
 
-export default class Buttons extends Component {
-    render() {
-        return (
-            <div className="keyboard">
-                <button id="clear">AC</button>
-                <button id="divide">/</button>
-                <button id="multiply">x</button>
-
-                <button id="seven">7</button>
-                <button id="eight">8</button>
-                <button id="nine">9</button>
-                <button id="subtract">-</button>
-
-                <button id="four">4</button>
-                <button id="five">5</button>
-                <button id="six">6</button>
-                <button id="add">+</button>
-
-                <button id="one">1</button>
-                <button id="two">2</button>
-                <button id="three">3</button>
-
-                <button id="zero">0</button>
-                <button id="decimal">.</button>
-                <button id="equals">=</button>
-            </div>
-        )
-    }
+export default function Buttons({id , number}) {
+    return (
+        <div className={id}>
+            <button id={id}>{number}</button>
+        </div>
+    )
 }
+
