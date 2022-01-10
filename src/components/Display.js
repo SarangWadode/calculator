cfe0426b2a8a08301dba8612c86eeb36830d310a
Sarang Wadode <sarangwadode@gmail.com>
2022-01-10 19:27:56 +0530

    styling display and keys

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Display( {display,formula}) {
     return (
-        <div>
+        <div className='display'>
             <div id="formula">{formula}</div>
             <div id="display">{display}</div>
         </div>
